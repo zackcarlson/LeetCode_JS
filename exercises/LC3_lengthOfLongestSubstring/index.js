31a8bc107f90eead36cfb9cceb0d3ae1c051b422
zackcarlson <zackt.carlson@gmail.com>
2019-06-11 10:49:53 -0700

    Solve function

--- exercises/LC3_lengthOfLongestSubstring/index.js
+++ exercises/LC3_lengthOfLongestSubstring/index.js
@@ -4,6 +4,23 @@
 // lengthOfLongestSubstring("abcabcbb") --> 3 since length of "abc"
 // lengthOfLongestSubstring("bbbbb") --> 1 since length of "b"
 
-function lengthOfLongestSubstring(s) {}
+function lengthOfLongestSubstring(s) {
+  let hash = {};
+  let start = 0;
+  let maxLen = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const endChar = s[i];
+
+    if (hash[endChar] >= start) {
+      start = hash[endChar] + 1;
+    }
+
+    hash[endChar] = i;
+    maxLen = Math.max(maxLen, i - start + 1);
+  }
+
+  return maxLen;
+}
 
 module.exports = lengthOfLongestSubstring;
