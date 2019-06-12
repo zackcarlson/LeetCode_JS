cb00277b1f9ba5ee3f7b31422b8db72a0ef316f4
zackcarlson <zackt.carlson@gmail.com>
2019-06-11 19:28:48 -0700

    Complete groupAnagrams

--- exercises/LC49_groupAnagrams/index.js
+++ exercises/LC49_groupAnagrams/index.js
@@ -1,3 +1,17 @@
-function groupAnagrams(strs) {}
+function groupAnagrams(strs) {
+  let grouped = {};
+
+  for (let str of strs) {
+    let group = str.split('').sort().join('');
+      
+    if (!grouped[group]) {
+      grouped[group] = [];
+    }
+
+    grouped[group].push(str);
+  }
+  
+  return Object.values(grouped);
+}
 
 module.exports = groupAnagrams;
