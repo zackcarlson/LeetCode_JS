b1b7dff02788a916b3bff4941300126d224dcd71
zackcarlson <zackt.carlson@gmail.com>
2019-06-11 18:29:44 -0700

    Solve longestPalindrome

--- exercises/LC5_longestPalindrome/index.js
+++ exercises/LC5_longestPalindrome/index.js
@@ -4,6 +4,30 @@
 // longestPalindrome("abba") --> "abba"
 // longestPalindrome("a") --> "a"
 
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+  let startIdx = 0;
+  let maxLength = 1;
+
+  function expandAroundMiddle(left, right) {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      const currentPalLength = right - left + 1;
+      if (currentPalLength > maxLength) {
+        maxLength = currentPalLength;
+        startIdx = left;
+      }
+
+      left--;
+      right++;
+    }
+  }
+
+  for (let i = 0; i < s.length; i++) {
+    expandAroundMiddle(i - 1, i + 1);
+    // accounts for even length strings
+    expandAroundMiddle(i, i + 1); 
+  }
+
+  return s.slice(startIdx, startIdx + maxLength);
+}
 
 module.exports = longestPalindrome;
