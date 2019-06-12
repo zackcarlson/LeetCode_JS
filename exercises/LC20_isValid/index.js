017e5741c0f8f7d9525cc20e5726de58463fd75e
zackcarlson <zackt.carlson@gmail.com>
2019-06-11 18:55:13 -0700

    Complete isValid

--- exercises/LC20_isValid/index.js
+++ exercises/LC20_isValid/index.js
@@ -1,3 +1,21 @@
-const isValid = s => {};
+const isValid = s => {
+  let stack = [];
+  let pairs = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
+  };
+
+  for (let char of s) {
+    if (pairs[char]) {
+      stack.push(char);
+    } else {
+      let open = stack.pop();
+      if (pairs[open] !== char) return false;
+    }
+  }
+
+  return stack.length === 0;
+};
 
 module.exports = isValid;
